d63f806b52eb3dc751ba7a39c81b793c4d4b99cd
Kenneth Cordero <kencordero@gmail.com>
2017-02-08 00:10:12 -0600

    Added skeletal HTML

---
+++ index.php
@@ -0,0 +1,11 @@
+<title>Kenneth Cordero</title>
+<h1>Kenneth Cordero</h1>
+<div id="about-me">
+    <p>About Me</p>
+</div>
+<div id="image">
+    <img alt="Me">
+</div>
+<div id="quote">
+    <p>Quote goes here</p>
+</div>
