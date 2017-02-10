890419f99e2e86e16051f091a7efc6f9b2d40075
Kenneth Cordero <kencordero@gmail.com>
2017-02-09 20:28:50 -0600

    Updated formatting

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php require('quotes.php') ?>
 <!doctype html>
+<html lang="en-us">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Kenneth Cordero</title>
 <link rel="shortcut icon" type="'image/x-icon" href="/images/favicon.ico">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="/styles/default.css">
 <div class="container">
     <h1>Kenneth Cordero</h1>
+    <hr>
     <div id="about-me">
-        <h2>About Me</h2>
+        <h4>About Me</h4>
         <p>Welcome to my site. I'm Ken and I live in Chicagoland with my wife and two kids.</p>
         <p>I enjoy playing classical guitar in my ever-diminishing spare time.</p>
     </div>
