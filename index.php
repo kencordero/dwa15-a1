790ee49bce348f511223df3e41d0642c40faf8c3
Kenneth Cordero <kencordero@gmail.com>
2017-02-09 20:15:40 -0600

    Added image assets

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <?php require('quotes.php') ?>
 <!doctype html>
 <title>Kenneth Cordero</title>
-<h1>Kenneth Cordero</h1>
-<div id="about-me">
-    <p>About Me</p>
-</div>
-<div id="image">
-    <img alt="Me">
-</div>
-<div id="quote">
-    <p><?= $random_quote ?></p>
+<link rel="shortcut icon" type="'image/x-icon" href="/images/favicon.ico">
+<div class="container">
+    <h1>Kenneth Cordero</h1>
+    <div id="about-me">
+        <h2>About Me</h2>
+        <p>Welcome to my site. I'm Ken and I live in Chicagoland with my wife and two kids.</p>
+        <p>I enjoy playing classical guitar in my ever-diminishing spare time.</p>
+    </div>
+    <div id="image">
+        <img src="/images/me.jpg" alt="Me">
+    </div>
+    <div id="quote">
+        <p><?= $random_quote ?></p>
+    </div>
 </div>
